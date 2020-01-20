7959da7cc00f9a9a1fbea96721d851cd04b4192b
James Page <JPAGE8@jaguarlandrover.com>
2020-01-20 10:05:31 +0000

    started creating the pyinquirer input sets

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from PyInquirer import prompt, print_json,ValidationError,Validator
+import datetime
+from pprint import pprint
+class DateValidator(Validator):
+    def validate(self,document):
+        try:
+            datetime.datetime.strptime(document.text, '%d-%m-%Y')
+            print(document.text)
+        except ValueError:
+            raise ValidationError(
+                message ="Incorrect data format, should be YYYY-MM-DD",
+                 cursor_position=len(document.text))
+
 class Coffee:
     def __init__(self,brand,R_date,R_taste,proccess,grind,B_method,C_in,C_out,T_complete,taste,notes):
         self.brand = brand
@@ -21,7 +34,62 @@ class Coffee:
             C_in_calc = float(self.C_in)*rat_calcd
             ratio = f"{C_in_calc}grams per 1000ml/1L"
         return ratio
-    
-roast1 = Coffee("Mor welsh coffee company","2/1/2020","","washed","1","espress","32.5","500","30",["tart","cherry","Full"],"notes section")
 
-print(roast1.ratio_calc())
+##roast1 = Coffee("Mor welsh coffee company","2/1/2020","","washed","1","espress","32.5","500","30",["tart","cherry","Full"],"notes section")
+
+questions = [
+    {
+        'type': 'input',
+        'name': 'brand',
+        'message': 'Enter Coffee name',
+    },
+
+    {
+        'type': 'input',
+        'name': 'R_date',
+        'message': 'Enter Roast date (DD-MM-YYYY)',
+        'validate': DateValidator
+    },
+    
+    {
+        'type': 'editor',
+        'name': 'R_taste',
+        'message': 'Please write the roasters taste notes',
+        'default': 'Roasters Taste Notes: ',
+        'eargs': {
+            'editor':'default',
+            'ext':'.txt'
+                }
+    },
+
+    {
+        'type':'list',
+        'name':'proccess',
+        'message':'Select the coffee proccess',
+        'choices':['Natural','Washed','Honey']
+    },
+
+    {
+        'type':'input',
+        'name':'grind',
+        'message':'Enter Grind setting/notes',
+    },
+
+        {
+        'type':'list',
+        'name':'B_method',
+        'message':'Select the Brew method',
+        'choices':['V60','Aeropress','Mellita Look IV','Espresso','French press','Stove top - Moka pot']
+    },
+
+    {
+        'type':'input',
+        'name':'C_in',
+        'message':'Enter how much coffee was used in grams'
+        #need validation it is a number
+    },
+]   
+
+
+answers = prompt(questions)
+pprint(answers)
