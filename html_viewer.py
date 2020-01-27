e6679cb9874a4aac4618f498536c47ed950a2f63
James Page <JPAGE8@jaguarlandrover.com>
2020-01-27 14:55:35 +0000

    Finished HTML VIEWER

--- html_viewer.py
+++ html_viewer.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def show_tables():
     Log_df = log_ToDict()
-
-    return render_template('view.html',tables=[Log_df.to_html(classes ='mystyle')])
+    rendered = render_template('view.html',tables=[Log_df.to_html(classes ='mystyle')])
+    return rendered
 
 app.run(debug=True)
